280d99211ac695555fdc9696f8e1cb02a3cff9b4
Marie <ashuramaru@tenjin-dk.com>
2023-03-21 14:55:29 +0200

    return nixos host directory

---
+++ host/home-server/programs/nodejs.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+{
+  config.environment.systemPackages = with pkgs.nodePackages; [ 
+    yarn # node package manager
+    typescript # tsc 
+    ts-node # repl for typescript
+    tsun # testing
+    javascript-typescript-langserver
+    nodemon # uptimer for js and not only apps
+    lodash # generic library mostly deprecated but let it be
+    jsdoc # jsdocuments generator 
+    sass # sass compiler 
+  ];
+}
