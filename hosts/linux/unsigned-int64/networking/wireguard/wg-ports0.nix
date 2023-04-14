dc66ed712021751573aaf221d047d3f69a8f4990
Marie <ashuramaru@tenjin-dk.com>
2023-04-14 17:35:06 +0300

    fix inheriting path

--- hosts/linux/unsigned-int64/networking/wireguard/wg-ports0.nix
+++ hosts/linux/unsigned-int64/networking/wireguard/wg-ports0.nix
@@ -4,11 +4,11 @@
     wg-ports0 = { 
       address = [ "172.168.10.1/24" ];
       listenPort = 51280;
-      privateKeyFile = config.age.wireguard-server.path;
+      privateKeyFile = config.age.secrets.wireguard-server.path;
       peers = [
         {
 	  publicKey = "QCg3hCNix8lMAw+l/icN7xRjmautUjMK6tqC+GzOg2I=";
-	  presharedKeyFile = config.age.wireguard-shared.path;
+	  presharedKeyFile = config.age.secrets.wireguard-shared.path;
 	  allowedIPs = [ "172.168.10.2/32" ];
 	}
       ];
