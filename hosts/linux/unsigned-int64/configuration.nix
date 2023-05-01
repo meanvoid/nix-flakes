374d9924e7a172444de77408ebbf1cb5b8ad8b83
Marie <ashuramaru@tenjin-dk.com>
2023-05-01 15:47:44 +0300

    testing nat with dnsmasq

--- hosts/linux/unsigned-int64/configuration.nix
+++ hosts/linux/unsigned-int64/configuration.nix
@@ -176,14 +176,14 @@
         permitRootLogin = "prohibit-password";
       };
     };
-    # dnsmasq = {
-    #   enable = true;
-    #   settings = {
-    #     inteface = "wireguard0";
-    #     server = [ "1.1.1.1" ];
-    #    };
-    #    resolveLocalQueries = true;
-    # };
+    dnsmasq = {
+      enable = true;
+      settings = {
+        inteface = "wireguard0";
+        server = [ "1.1.1.1" ];
+       };
+       resolveLocalQueries = true;
+    };
   };
   programs = {
     gnupg.agent = {
