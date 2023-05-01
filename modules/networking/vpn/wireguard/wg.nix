8f736a97a753d613d96c8adac5b9e7972254bb6f
Marie <ashuramaru@tenjin-dk.com>
2023-05-01 16:08:30 +0300

    add killswitch

--- modules/networking/vpn/wireguard/wg.nix
+++ modules/networking/vpn/wireguard/wg.nix
@@ -19,13 +19,13 @@ in
       }];
     };
     wg0 = {
-      address = [ "192.168.10.100/24" ];
+      address = [ "192.168.10.100/24" "dced:2718:5f06:718a::1/64" ];
       # dns = [ "192.168.10.1" "dced:2718:5f06:718a::1" ];
       privateKeyFile = private;
       peers = [{
         publicKey = "HU4h1/FqzNs5C6Vo5bXKVEE2bDq5PQe2LgINZywcpic=";
         presharedKeyFile = shared;
-        allowedIPs = [ "192.168.10.1/32" ];
+        allowedIPs = [ "0.0.0.0/0" "::/0" ];
         endpoint = "tenjin-dk.com:51820";
         persistentKeepalive = 25;      
       }];
