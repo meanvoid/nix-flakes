7cef2ef86b4ed848f444006b9b0ad699a7b1872f
Marie <ashuramaru@tenjin-dk.com>
2023-06-02 18:13:43 +0300

    added hdparm systemd timer

--- modules/unsigned-int32/services/utils/hddparm.nix
+++ modules/unsigned-int32/services/utils/hddparm.nix
@@ -3,14 +3,35 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  cfg = config.powerManagement;
+in {
+  powerManagement.enable = true;
   powerManagement.powerUpCommands = ''
-    for dev in $(
-      lsblk -d -o name,rota |
-      grep ' 1$' |
-      awk '{print $1}'
-    ); do
-      ${pkgs.hdparm}/bin/hdparm -S 120 -B 127 /dev/$dev;
-    done
+    ${pkgs.bash}/bin/bash -c '
+      ${pkgs.hdparm}/sbin/hdparm -S 9 -B 127 $(
+        ${pkgs.utillinux}/bin/lsblk -dnp -o name,rota |
+        ${pkgs.gnugrep}/bin/grep '.*\s1' |
+        ${pkgs.coreutils}/bin/cut -d ' ' -f 1
+    )'
   '';
+  powerManagement.powerDownCommands = ''
+    ${pkgs.hdparm}/sbin/hdparm -B 255
+  '';
+
+  systemd.services.hdparm-sleep = {
+    description = "Suspends all rotating disks";
+    after = ["suspend.target" "hibernate.target" "hybrid-sleep.target" "suspend-then-hibernate.target"];
+    script = ''${cfg.powerUpCommands}'';
+    serviceConfig.Type = "oneshot";
+    serviceConfig.User = "root";
+  };
+  systemd.timers.myPowerUpTimer = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnCalendar = "*-*-* 00:00:00";
+      AccuracySec = "1m";
+      unit = "hdparm-sleep.service";
+    };
+  };
 }
