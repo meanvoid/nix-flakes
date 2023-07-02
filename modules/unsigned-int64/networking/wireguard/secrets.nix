fe4223fa37a7a3062953798a82763d91e5498612
Mariia Holovata <ashuramaru@riseup.net>
2023-07-02 16:16:26 +0300

    fix unavailable apps on newer nextcloud 27 version

--- modules/unsigned-int64/networking/wireguard/secrets.nix
+++ modules/unsigned-int64/networking/wireguard/secrets.nix
@@ -7,7 +7,7 @@
   ...
 }: {
   age.secrets = {
-    wireguard-server.file = path + /secrets/wireguard0.age;
+    wireguard-server.file = path + /secrets/wireguard-server.age;
     wireguard-shared.file = path + /secrets/wireguard-shared.age;
     wireguard-shared_fumono.file = path + /secrets/wireguard-shared_fumono;
   };
