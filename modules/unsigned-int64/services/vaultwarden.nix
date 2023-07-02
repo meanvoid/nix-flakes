31e40fa2b1a1cae0b02a3edaeea0e528967fdb67
Mariia Holovata <ashuramaru@riseup.net>
2023-07-02 15:04:24 +0300

    update secrets

--- modules/unsigned-int64/services/vaultwarden.nix
+++ modules/unsigned-int64/services/vaultwarden.nix
@@ -1,25 +1,47 @@
 {
-  config,
   lib,
+  config,
   pkgs,
+  agenix,
   ...
 }: let
   domain = "bitwarden.tenjin-dk.com";
 in {
+  age.secrets.vaultwarden-env = {
+    file = path + /secrets/vaultwarden-env.age;
+    path = "/var/lib/backup/.env";
+    mode = "770";
+    owner = "vaultwarden";
+    group = "vaultwarden";
+  };
+
   services.vaultwarden = {
     enable = true;
     package = pkgs.vaultwarden-postgresql;
+    backupDir = "/var/lib/backup/bitwarden";
+    environmentFile = config.age.secrets.vaultwarden-env.path;
+
     config = {
       domain = "https://bitwarden.tenjin-dk.com";
-      signupsAllowed = false;
-      signupsVerify = true;
-      signupsDomainsWhitelist = "tenjin-dk.com, riseup.net, meanrin.cat";
-      websocketEnabled = true;
-      websocketAddress = "0.0.0.0";
-      websoketPort = "3012";
-      enableDbWal = false;
+      rocketAddress = "127.0.0.1";
       rocketPort = 8812;
       rocketLog = "critical";
+
+      websocketEnabled = true;
+      websocketAddress = "0.0.0.0";
+      websocketPort = "3012";
+
+      enableDbWal = true;
+
+      signupsAllowed = false;
+      signupsVerify = true;
+      signupsDomainsWhitelist = "fumoposting.com, tenjin-dk.com, riseup.net, meanrin.cat";
+
+      smtpHost = "127.0.0.1";
+      smtpPort = 587;
+      smtpSSL = true;
+      smtpFrom = "no-reply@cloud.tenjin-dk.com";
+      smtpFromName = "[tenjin-dk.com] bitwarden server";
     };
   };
   services.nginx.virtualHosts."${domain}" = {
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,18 +4,18 @@ let
     meanrin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFS10V6yIxDfxjepo1jmYam1qQ67NMpkPeUFDJokhOJj";
   };
   systems = {
-    unsigned-int8 = "";
-    unsigned-int32 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILn6KuA2ztDSI2vFwOgZlmvEik4pkVJ1pvIKBMo6rh/4";
-    unsigned-int64 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEuPO55kimOLcT8ZcF6jmLSq0ET9YcKa1gqvTpGEoTR1";
+    unsigned-int16 = "";
+    unsigned-int32 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILn6KuA2ztDSI2vFwOgZlmvEik4pkVJ1pvIKBMo6rh/4 root@unsigned-int32";
+    unsigned-int64 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdiVn6zgj+VGj3BIGiwMFH3AumoGSCzaVukcbQWVz1K root@unsigned-int64";
   };
 in {
   "wireguard-client.age".publicKeys = [systems.unsigned-int32];
+  "wireguard-server.age".publicKeys = [systems.unsigned-int32 systems.unsigned-int64];
   "wireguard-shared.age".publicKeys = [systems.unsigned-int32 systems.unsigned-int64];
-  "wireguard-server.age".publicKeys = [systems.unsigned-int64];
-  "wireguard0.age".publicKeys = [systems.unsigned-int64];
-  "wireguard-shared_signed.age".publicKeys = [systems.unsigned-int64];
-  "wireguard-server-shared_twi.age".publicKeys = [systems.unsigned-int64];
-  "wireguard-server-shared_julio.age".publicKeys = [systems.unsigned-int64];
-  "admin.age".publicKeys = [systems.unsigned-int64];
+  "wireguard-shared_fumono.age".publicKeys = [systems.unsigned-int32 systems.unsigned-int64];
+
   "njalla-api-token.age".publicKeys = [systems.unsigned-int32 systems.unsigned-int64];
+  "proxy.age".publicKeys = [systems.unsigned-int64];
+  "admin.age".publicKeys = [systems.unsigned-int64];
+  "vaultwarden-env.age".publicKeys = [systems.unsigned-int64];
 }
