b1ce10c12d2811851f4638bcbdebe98eee866dc8
Mariia Holovata <ashuramaru@riseup.net>
2023-07-02 18:12:22 +0300

    try msmtp with updated config

---
+++ modules/unsigned-int64/services/msmtp.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  config,
+  pkgs,
+  agenix,
+  path,
+  ...
+}: {
+  age.secrets.mail = {
+    file = path + /secrets/mail.age;
+    mode = "770";
+    owner = "mail";
+    group = "mail";
+  };
+  users.groups.mail = {
+    gid = config.users.users.mail.uid;
+    members = ["nextcloud" "vaultwarden"];
+  };
+  users.users.mail = {
+    uid = 4322;
+    isSystemUser = true;
+    group = "mail";
+    extraGroups = ["nextcloud" "vaultwarden"];
+  };
+  programs.msmtp = {
+    enable = true;
+    accounts = {
+      default = {
+        auth = true;
+        tls = true;
+        tlsStarttls = true;
+        port = 587;
+        host = "antila.uberspace.de";
+        from = "no-reply@cloud.tenjin-dk.com";
+        user = "no-reply@cloud.tenjin-dk.com";
+        passwordeval = "cat ${toString config.age.secrets.mail.path}";
+      };
+    };
+  };
+}
