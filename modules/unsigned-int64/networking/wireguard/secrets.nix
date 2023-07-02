8e25d36916b28237406aed5e5b0ebd37c26e90b3
Mariia Holovata <ashuramaru@riseup.net>
2023-07-02 16:17:12 +0300

    fix secrets

--- modules/unsigned-int64/networking/wireguard/secrets.nix
+++ modules/unsigned-int64/networking/wireguard/secrets.nix
@@ -9,6 +9,6 @@
   age.secrets = {
     wireguard-server.file = path + /secrets/wireguard-server.age;
     wireguard-shared.file = path + /secrets/wireguard-shared.age;
-    wireguard-shared_fumono.file = path + /secrets/wireguard-shared_fumono;
+    wireguard-shared_fumono.file = path + /secrets/wireguard-shared_fumono.age;
   };
 }
