aa2e79569ee0ad8123e66255f13b7c0b3dbaf3c0
Mariia Holovata <ashuramaru@tenjin-dk.com>
2023-07-03 20:39:52 +0300

    remove grafana temp to make sure that database is actually exists

--- modules/unsigned-int64/services/grafana.nix
+++ modules/unsigned-int64/services/grafana.nix
@@ -2,8 +2,18 @@
   config,
   lib,
   pkgs,
+  agenix,
+  path,
   ...
 }: {
+  age.secrets.grafana_pgsql = {
+    file = path + /secrets/grafana_pgsql.age;
+    path = "/var/lib/backup/pgsql.pass";
+    mode = "0640";
+    owner = "grafana";
+    group = "grafana";
+  };
+
   services.grafana = {
     enable = true;
     settings = {
@@ -17,7 +27,15 @@
         root_url = "%(protocol)s://%(domain)s:%(http_port)s/grafana/";
         serve_from_sub_path = true;
       };
-      # database = {};
+      database = {
+        type = "postgresql";
+        name = "grafana";
+        user = "grafana";
+        password = "$__file{${config.age.secrets.grafana_pgsql.path}}";
+        ssl_mode = "verify-full";
+        host = "127.0.0.1:5432";
+        log_queries = true;
+      };
     };
   };
   services.prometheus = {
@@ -31,20 +49,17 @@
       };
     };
   };
-  services.nginx.virtualHosts = {
-    # "${config.services.grafana.settings.server.domain}" = {
-    "metrics.tenjin-dk.com" = {
-      enableACME = true;
-      forceSSL = true;
-      locations = {
-        "/grafana/" = {
-          proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
-          proxyWebsockets = true;
-        };
-        "/grafana/api/live/" = {
-          proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
-          proxyWebsockets = true;
-        };
+  services.nginx.virtualHosts."${config.services.grafana.settings.server.domain}" = {
+    enableACME = true;
+    forceSSL = true;
+    locations = {
+      "/grafana/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
+        proxyWebsockets = true;
+      };
+      "/grafana/api/live/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
+        proxyWebsockets = true;
       };
     };
   };
