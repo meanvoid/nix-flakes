c610b28425b9f8ff07344a163ff5d1433bf0afcf
Mariia Holovata <ashuramaru@tenjin-dk.com>
2023-07-04 12:15:00 +0300

    test loki

--- modules/unsigned-int64/services/grafana.nix
+++ modules/unsigned-int64/services/grafana.nix
@@ -51,17 +51,22 @@
         default_language = "en_us";
       };
     };
+    declarativePlugins = with pkgs.grafanaPlugins; [
+      grafana-piechart-panel
+    ];
   };
   services.prometheus = {
     enable = true;
     webExternalUrl = "/metrics/";
     webConfigFile = path + /modules/unsigned-int64/services/config.yml;
-    port = 9001;
+    port = 9000;
     exporters = {
       node = {
         enable = true;
-        enabledCollectors = ["systemd"];
-        port = 9002;
+        enabledCollectors = [
+          "systemd"
+        ];
+        port = 9100;
       };
     };
     scrapeConfigs = [
@@ -75,6 +80,118 @@
       }
     ];
   };
+  services.loki = {
+    enable = true;
+    configuration = {
+      server.http_listen_port = 3030;
+      auth_enabled = false;
+
+      ingester = {
+        lifecycler = {
+          address = "127.0.0.1";
+          ring = {
+            kvstore = {
+              store = "inmemory";
+            };
+            replication_factor = 1;
+          };
+        };
+        chunk_idle_period = "1h";
+        max_chunk_age = "1h";
+        chunk_target_size = 999999;
+        chunk_retain_period = "30s";
+        max_transfer_retries = 0;
+      };
+
+      schema_config = {
+        configs = [
+          {
+            from = "2022-06-06";
+            store = "boltdb-shipper";
+            object_store = "filesystem";
+            schema = "v11";
+            index = {
+              prefix = "index_";
+              period = "24h";
+            };
+          }
+        ];
+      };
+
+      storage_config = {
+        boltdb_shipper = {
+          active_index_directory = "/var/lib/loki/boltdb-shipper-active";
+          cache_location = "/var/lib/loki/boltdb-shipper-cache";
+          cache_ttl = "24h";
+          shared_store = "filesystem";
+        };
+
+        filesystem = {
+          directory = "/var/lib/loki/chunks";
+        };
+      };
+
+      limits_config = {
+        reject_old_samples = true;
+        reject_old_samples_max_age = "168h";
+      };
+
+      chunk_store_config = {
+        max_look_back_period = "0s";
+      };
+
+      table_manager = {
+        retention_deletes_enabled = false;
+        retention_period = "0s";
+      };
+
+      compactor = {
+        working_directory = "/var/lib/loki";
+        shared_store = "filesystem";
+        compactor_ring = {
+          kvstore = {
+            store = "inmemory";
+          };
+        };
+      };
+    };
+  };
+  services.promtail = {
+    enable = true;
+    configuration = {
+      server = {
+        http_listen_port = 3031;
+        grpc_listen_port = 0;
+      };
+      positions = {
+        filename = "/tmp/positions.yaml";
+      };
+      clients = [
+        {
+          url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
+        }
+      ];
+      scrape_configs = [
+        {
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = "unsigned-int64";
+            };
+          };
+          relabel_configs = [
+            {
+              source_labels = ["__journal__systemd_unit"];
+              target_label = "unit";
+            }
+          ];
+        }
+      ];
+    };
+  };
+
   services.nginx.virtualHosts."${config.services.grafana.settings.server.domain}" = {
     enableACME = true;
     forceSSL = true;
@@ -89,6 +206,14 @@
       };
       "/metrics/" = {
         proxyPass = "http://127.0.0.1:${toString config.services.prometheus.port}";
+        proxyWebsockets = true;
+      };
+      "/loki/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}";
+        proxyWebsockets = true;
+      };
+      "/loki/api/v1/push" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
         proxyWebsockets = true;
       };
     };
