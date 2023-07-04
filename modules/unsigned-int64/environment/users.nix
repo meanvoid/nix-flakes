6f9e146853f16060758eaaba4e10f51b7d9a0b0b
Mariia Holovata <ashuramaru@tenjin-dk.com>
2023-07-04 21:38:41 +0300

    fix uid

--- modules/unsigned-int64/environment/users.nix
+++ modules/unsigned-int64/environment/users.nix
@@ -36,7 +36,7 @@
       home = "/Users/ashuramaru";
       description = "Marie";
       initialHashedPassword = "";
-      extraGroups = ["wheel" "minecraft"];
+      extraGroups = ["ashuramaru" "wheel" "minecraft"];
       openssh.authorizedKeys.keys = [
         "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBNR1p1OviZgAkv5xQ10NTLOusPT8pQUG2qCTpO3AhmxaZM2mWNePVNqPnjxNHjWN+a/FcZ5on74QZQJtwXI5m80AAAAOc3NoOnJlbW90ZS1kc2E= email:ashuramaru@tenjin-dk.com id:ashuramaru@unsigned-int32"
       ];
@@ -46,7 +46,7 @@
       home = "/Users/meanrin";
       description = "Alex";
       initialHashedPassword = "";
-      extraGroups = ["wheel"];
+      extraGroups = ["meanrin" "wheel" "minecraft"];
       openssh.authorizedKeys.keys = [
         "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBCzoNOzhhF9uYDu7CbuzVRJ2K6dClXLrEoJrQvIYjnxHTBMqKuByi9M2HEmkpGO+a3H3WjeeXfqjH2CwZJ97jmIAAAAEc3NoOg== meanrin@outlook.com"
       ];
@@ -56,7 +56,7 @@
       home = "/Users/fumono";
       description = "Fumono";
       initialHashedPassword = "";
-      extraGroups = ["docker" "podman" "minecraft"];
+      extraGroups = ["fumono" "docker" "podman" "minecraft"];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHBeBj6thLiVFNGZI1NuTHKIPvh332Szad2zsgjdzhR mc-server"
       ];
