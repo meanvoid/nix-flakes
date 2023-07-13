70187464dc70d35c1e76473c2b1d4bb416cf5831
Mariia Holovata <ashuramaru@tenjin-dk.com>
2023-07-13 19:31:48 +0300

    add and update flatpak apps

--- hosts/unsigned-int32/home/ashuramaru/programs/misc/flatpak.nix
+++ hosts/unsigned-int32/home/ashuramaru/programs/misc/flatpak.nix
@@ -12,35 +12,47 @@
       "flathub-beta" = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
     };
     packages = [
-      "flathub:com.github.tchx84.Flatseal//stable"
-      "flathub:org.kde.index//stable"
-      "flathub-beta:org.kde.kdenlive//stable"
-      "flathub:com.discordapp.Discord//stable"
-      "flathub:de.shorsh.discord-screenaudio//stable"
-      "flathub:com.github.Matoking.protontricks//stable"
-      "flathub:com.nextcloud.desktopclient.nextcloud//stable"
-      "flathub:com.obsproject.Studio//stable"
-      "flathub:com.obsproject.Studio.Plugin.GStreamerVaapi//stable"
-      "flathub:com.obsproject.Studio.Plugin.Gstreamer//stable"
-      "flathub:com.obsproject.Studio.Plugin.InputOverlay//stable"
-      "flathub:com.obsproject.Studio.Plugin.NVFBC//stable"
-      "flathub:com.obsproject.Studio.Plugin.OBSVkCapture//stable"
-      "flathub:com.obsproject.Studio.Plugin.SceneSwitcher//stable"
-      "flathub:com.obsproject.Studio.Plugin.WebSocket//stable"
-      "flathub:com.usebottles.bottles//stable"
-      "flathub:info.cemu.Cemu//stable"
-      "flathub:com.valvesoftware.Steam.CompatibilityTool.Proton//stable"
-      "flathub:com.valvesoftware.Steam.CompatibilityTool.Proton-GE//stable"
-      "flathub:io.github.Foldex.AdwSteamGtk//stable"
-      "flathub:io.github.arunsivaramanneo.GPUViewer//stable"
-      "flathub:io.gitlab.theevilskeleton.Upscaler//stable"
-      "flathub:net.davidotek.pupgui2//stable"
-      "flathub:sh.ppy.osu//stable"
-      "flathub:org.freedesktop.Platform.VulkanLayer.MangoHud//stable"
-      "flathub:org.freedesktop.Platform.VulkanLayer.OBSVkCapture//stable"
-      "flathub:org.freedesktop.Platform.VulkanLayer.vkBasalt//stable"
-      "flathub:org.gtk.Gtk3theme.Adwaita-dark//stable"
-      "flathub:org.gtk.Gtk3theme.adw-gtk3-dark//stable"
+      # KDE/Qt
+      "flathub:com.github.tchx84.Flatseal/x86_64/stable"
+      "flathub:org.kde.index/x86_64/stable"
+      "flathub:org.kde.dolphin/x86_64/stable"
+      "flathub:org.kde.kdenlive/x86_64/stable"
+      "flathub:de.shorsh.discord-screenaudio/x86_64/stable"
+
+      # Desktop
+      "flathub:com.discordapp.Discord/x86_64/stable"
+      "flathub:com.github.Matoking.protontricks/x86_64/stable"
+      "flathub-beta:com.nextcloud.desktopclient.nextcloud/x86_64/stable"
+      "flathub:io.github.arunsivaramanneo.GPUViewer/x86_64/stable"
+      "flathub:io.gitlab.theevilskeleton.Upscaler/x86_64/stable"
+
+      # OBS
+      "flathub:com.obsproject.Studio/x86_64/stable"
+      "flathub:com.obsproject.Studio.Plugin.GStreamerVaapi/x86_64/stable"
+      "flathub:com.obsproject.Studio.Plugin.Gstreamer/x86_64/stable"
+      "flathub:com.obsproject.Studio.Plugin.InputOverlay/x86_64/stable"
+      "flathub:com.obsproject.Studio.Plugin.NVFBC/x86_64/stable"
+      "flathub:com.obsproject.Studio.Plugin.OBSVkCapture/x86_64/stable"
+      "flathub:com.obsproject.Studio.Plugin.SceneSwitcher/x86_64/stable"
+      "flathub:com.obsproject.Studio.Plugin.WebSocket/x86_64/stable"
+      "flathub:org.freedesktop.Platform.VulkanLayer.OBSVkCapture/x86_64/stable"
+
+      # Gaming
+      "flathub:com.usebottles.bottles/x86_64/stable"
+      "flathub:sh.ppy.osu/x86_64/stable"
+      "flathub:io.github.Foldex.AdwSteamGtk/x86_64/stable"
+      "flathub:net.davidotek.pupgui2/x86_64/stable"
+      "flathub:com.valvesoftware.Steam.CompatibilityTool.Proton/x86_64/stable"
+      "flathub:com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable"
+
+      # Vulkan utils
+      "flathub:org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/stable"
+      "flathub:org.freedesktop.Platform.VulkanLayer.vkBasalt/x86_64/stable"
+
+      # Utils
+      "flathub:org.gtk.Gtk3theme.Adwaita-dark/x86_64/stable"
+      "flathub:org.gtk.Gtk3theme.adw-gtk3-dark/x86_64/stable"
+      "flathub:org.gtk.Gtk3theme.Catppuccin-Mocha-Lavender/x86_64/stable"
     ];
   };
 }
