a74d6c54868eb25fa3186fccd05d14366c532845
Mariia Holovata <ashuramaru@tenjin-dk.com>
2023-07-13 21:56:38 +0300

    fix kde.platform

--- modules/unsigned-int32/programs/flatpak.nix
+++ modules/unsigned-int32/programs/flatpak.nix
@@ -13,7 +13,7 @@
     };
     packages = [
       "flathub:org.kde.Platform/x86_64/5.15-22.08"
-      "flathub:org.kde.Platform.QGnomePlatform/x86_64/5.15-22.08"
+      "flathub:org.kde.PlatformTheme.QGnomePlatform/x86_64/5.15-22.08"
       "flathub:org.kde.WaylandDecoration.QGnomePlatform-decoration/x86_64/5.15-22.08"
     ];
   };
