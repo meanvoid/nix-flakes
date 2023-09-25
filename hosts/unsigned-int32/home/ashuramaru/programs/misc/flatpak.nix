139332d2f3b9a35c639362e7d74b634a04a0f8b4
Mariia Holovata <ashuramaru@riseup.net>
2023-09-25 22:35:41 +0300

    flatpak merge

--- hosts/unsigned-int32/home/ashuramaru/programs/misc/flatpak.nix
+++ hosts/unsigned-int32/home/ashuramaru/programs/misc/flatpak.nix
@@ -13,45 +13,45 @@
     };
     packages = [
       # KDE/Qt
-      "flathub:com.github.tchx84.Flatseal/x86_64/stable"
-      "flathub:org.kde.index/x86_64/stable"
-      "flathub:org.kde.kdenlive/x86_64/stable"
-      "flathub:de.shorsh.discord-screenaudio/x86_64/stable"
+      "flathub:app/com.github.tchx84.Flatseal/x86_64/stable"
+      "flathub:app/org.kde.index/x86_64/stable"
+      "flathub:app/org.kde.kdenlive/x86_64/stable"
+      "flathub:app/de.shorsh.discord-screenaudio/x86_64/stable"
 
       # Desktop
-      "flathub:com.discordapp.Discord/x86_64/stable"
-      "flathub:com.github.Matoking.protontricks/x86_64/stable"
-      "flathub:com.nextcloud.desktopclient.nextcloud/x86_64/stable"
-      "flathub:io.github.arunsivaramanneo.GPUViewer/x86_64/stable"
-      "flathub:io.gitlab.theevilskeleton.Upscaler/x86_64/stable"
+      "flathub:app/com.discordapp.Discord/x86_64/stable"
+      "flathub:app/com.github.Matoking.protontricks/x86_64/stable"
+      "flathub:app/com.nextcloud.desktopclient.nextcloud/x86_64/stable"
+      "flathub:app/io.github.arunsivaramanneo.GPUViewer/x86_64/stable"
+      "flathub:app/io.gitlab.theevilskeleton.Upscaler/x86_64/stable"
 
       # OBS
-      "flathub:com.obsproject.Studio/x86_64/stable"
-      "flathub:com.obsproject.Studio.Plugin.GStreamerVaapi/x86_64/stable"
-      "flathub:com.obsproject.Studio.Plugin.Gstreamer/x86_64/stable"
-      "flathub:com.obsproject.Studio.Plugin.InputOverlay/x86_64/stable"
-      "flathub:com.obsproject.Studio.Plugin.OBSVkCapture/x86_64/stable"
-      "flathub:com.obsproject.Studio.Plugin.SceneSwitcher/x86_64/stable"
-      "flathub:com.obsproject.Studio.Plugin.WebSocket/x86_64/stable"
-      "flathub:org.freedesktop.Platform.VulkanLayer.OBSVkCapture/x86_64/22.08"
+      "flathub:app/com.obsproject.Studio/x86_64/stable"
+      "flathub:runtime/com.obsproject.Studio.Plugin.GStreamerVaapi/x86_64/stable"
+      "flathub:runtime/com.obsproject.Studio.Plugin.Gstreamer/x86_64/stable"
+      "flathub:runtime/com.obsproject.Studio.Plugin.InputOverlay/x86_64/stable"
+      "flathub:runtime/com.obsproject.Studio.Plugin.OBSVkCapture/x86_64/stable"
+      "flathub:runtime/com.obsproject.Studio.Plugin.SceneSwitcher/x86_64/stable"
+      "flathub:runtime/com.obsproject.Studio.Plugin.WebSocket/x86_64/stable"
+      "flathub:runtime/org.freedesktop.Platform.VulkanLayer.OBSVkCapture/x86_64/22.08"
 
       # Gaming
-      "flathub:com.usebottles.bottles/x86_64/stable"
-      "flathub:sh.ppy.osu/x86_64/stable"
-      "flathub:io.github.Foldex.AdwSteamGtk/x86_64/stable"
-      "flathub:net.davidotek.pupgui2/x86_64/stable"
-      "flathub:com.valvesoftware.Steam.CompatibilityTool.Proton/x86_64/stable"
-      "flathub:com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable"
-      "flathub:com.valvesoftware.Steam.Utility.gamescope/x86_64/stable"
+      "flathub:app/com.usebottles.bottles/x86_64/stable"
+      "flathub:app/sh.ppy.osu/x86_64/stable"
+      "flathub:app/io.github.Foldex.AdwSteamGtk/x86_64/stable"
+      "flathub:app/net.davidotek.pupgui2/x86_64/stable"
+      "flathub:app/com.valvesoftware.Steam.CompatibilityTool.Proton/x86_64/stable"
+      "flathub:app/com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable"
+      "flathub:app/com.valvesoftware.Steam.Utility.gamescope/x86_64/stable"
 
       # Vulkan utils
-      "flathub:org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/22.08"
-      "flathub:org.freedesktop.Platform.VulkanLayer.vkBasalt/x86_64/22.08"
+      "flathub:runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/22.08"
+      "flathub:runtime/org.freedesktop.Platform.VulkanLayer.vkBasalt/x86_64/22.08"
 
       # Utils
-      "flathub:org.gtk.Gtk3theme.Adwaita-dark/x86_64/3.22"
-      "flathub:org.gtk.Gtk3theme.adw-gtk3-dark/x86_64/3.22"
-      "flathub:org.gtk.Gtk3theme.Catppuccin-Mocha-Lavender/x86_64/3.22"
+      "flathub:runtime/org.gtk.Gtk3theme.Adwaita-dark/x86_64/3.22"
+      "flathub:runtime/org.gtk.Gtk3theme.adw-gtk3-dark/x86_64/3.22"
+      "flathub:runtime/org.gtk.Gtk3theme.Catppuccin-Mocha-Lavender/x86_64/3.22"
     ];
   };
 }
