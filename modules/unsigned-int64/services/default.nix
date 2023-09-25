fcb16a049414e4d5523945104ab1d515123180b1
Mariia Holovata <ashuramaru@riseup.net>
2023-09-25 19:38:08 +0300

    remove obsolete server and update new one

--- modules/unsigned-int64/services/default.nix
+++ modules/unsigned-int64/services/default.nix
@@ -3,9 +3,9 @@
   ./fail2ban.nix
   ./postgresql.nix
   ./nginx.nix
-  # ./nextcloud.nix
-  # ./vaultwarden.nix
-  # ./grafana.nix
-  # ./jellyfin.nix
+  ./nextcloud.nix
+  ./vaultwarden.nix
+  ./grafana.nix
+  ./jellyfin.nix
   ./transmission.nix
 ]
