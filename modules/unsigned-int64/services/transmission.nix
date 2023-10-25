cfb5476b0abe8d0d857845dc1fb188cb87c19f3b
Mariia Holovata <ashuramaru@riseup.net>
2023-10-25 12:13:44 +0300

    update nginx configuration

--- modules/unsigned-int64/services/transmission.nix
+++ modules/unsigned-int64/services/transmission.nix
@@ -42,14 +42,14 @@
     sslCertificate = "/var/lib/scerts/lib.tenjin-dk.com/lib.tenjin-dk.com.crt";
     sslCertificateKey = "/var/lib/scerts/lib.tenjin-dk.com/lib.tenjin-dk.com.key";
     locations."/" = {
-      proxyPass = "http://172.168.10.1:18765/transmission/";
+      proxyPass = "http://172.168.10.1:18765";
       proxyWebsockets = true;
       extraConfig = ''
         proxy_redirect off;
       '';
     };
     locations."/private/" = {
-      proxyPass = "http://172.168.10.1:9091/transmission/";
+      proxyPass = "http://172.168.10.1:9091";
       proxyWebsockets = true;
       extraConfig = ''
         proxy_redirect off;
