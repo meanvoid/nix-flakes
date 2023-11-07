643a95594f73530ab5f2e37bb16843d137349f85
Mariia Holovata <ashuramaru@riseup.net>
2023-11-07 19:53:56 +0200

    try to fix md disc

--- modules/unsigned-int64/networking/settings.nix
+++ modules/unsigned-int64/networking/settings.nix
@@ -33,15 +33,9 @@
       interface = "eno1";
     };
     nameservers = [
-      "127.0.0.1"
-      "::1"
+      "213.133.100.98"
+      "2a01:4f8:0:1::add:1098"
     ];
-    # extraHosts = ''
-    #   172.16.31.1 prom.tenjin-dk.com
-    #   172.16.31.1 public.tenjin-dk.com
-    #   172.16.31.1 private.tenjin-dk.com
-    #   172.16.31.1 lib.tenjin-dk.com
-    # '';
     nat = {
       enable = true;
       enableIPv6 = true;
@@ -130,74 +124,70 @@
           addr = "[dced:2718:5f06:718a::1]";
           port = 22;
         }
-        # wireguard0#public
-        {
-          addr = "10.64.10.1";
-          port = 43131;
-        }
-        {
-          addr = "[fd02:f8eb:7ca4:5f4c::1]";
-          port = 43131;
-        }
         # wireguard1
         {
           addr = "172.16.31.1";
           port = 22;
         }
         {
-          addr = "[f9c4:6fa6:98a2:a39c::1]";
+          addr = "[fd17:216b:31bc:1::1]";
           port = 22;
         }
       ];
     };
-    unbound = {
-      enable = true;
-      enableRootTrustAnchor = true;
-      resolveLocalQueries = true;
-      settings = {
-        server = {
-          interface = [
-            "127.0.0.1"
-          ];
-          private-domain = [
-            "remote.tenjin-dk.com."
-            "remote.fumoposting.com."
-          ];
-        };
-        private-address = [
-          "10.0.0.0/8"
-          "172.16.0.0/12"
-          "fd00::/8"
-          "fd17::/16"
-          "fe80::/10"
-        ];
-        local-zone = [
-          "1.31.16.172.in-addr.arpa. transparent"
-          "unsigned-int64.remote. transparent"
-        ];
-        local-data-ptr = [
-          "1.31.16.172.in-addr.arpa. unsigned-int64.remote"
-        ];
-        local-data = [
-          # A/AAAA records
-          "unsigned-int64.remote. IN A 172.16.31.1 3600"
-          "unsigned-int64.remote. IN AAAA fd17:216b:31bc:1::1 3600"
+    resolved.enable = true;
+    # unbound = {
+    #   enable = true;
+    #   enableRootTrustAnchor = true;
+    #   resolveLocalQueries = true;
+    #   settings = {
+    #     server = {
+    #       verbosity = 2;
+    #       interface = [
+    #         "127.0.0.1"
+    #         "::1"
+    #         "172.16.31.1"
+    #         fd17:216b:31bc:1::1
+    #       ];
+    #       private-domain = [
+    #         "remote.tenjin-dk.com."
+    #         "remote.fumoposting.com."
+    #       ];
+    #     };
+    #     private-address = [
+    #       "10.0.0.0/8"
+    #       "172.16.0.0/12"
+    #       "fd00::/8"
+    #       "fd17::/16"
+    #       "fe80::/10"
+    #     ];
+    #     local-zone = [
+    #       "1.31.16.172.in-addr.arpa transparent"
+    #       "unsigned-int64.remote transparent"
+    #     ];
+    #     local-data-ptr = [
+    #       "1.31.16.172.in-addr.arpa unsigned-int64.remote"
+    #     ];
+    #     local-data = [
+    #       # A/AAAA records
+    #       "unsigned-int64.remote IN A 172.16.31.1 3600"
+    #       "unsigned-int64.remote IN AAAA fd17:216b:31bc:1::1 3600"
 
-          # CNAME
-          "prom.tenjin-dk.com IN CNAME unsigned-int64.remote 3600"
-          "lib.tenjin-dk.com IN CNAME unsigned-int64.remote 3600"
-          "private.tenjin-dk.com IN CNAME unsigned-int64.remote 3600"
-          "public.tenjin-dk.com IN CNAME unsigned-int64.remote 3600"
+    #       # CNAME
+    #       "prom.tenjin-dk.com IN CNAME unsigned-int64.remote 3600"
+    #       "lib.tenjin-dk.com IN CNAME unsigned-int64.remote 3600"
+    #       "private.tenjin-dk.com IN CNAME unsigned-int64.remote 3600"
+    #       "public.tenjin-dk.com IN CNAME unsigned-int64.remote 3600"
 
-          # PTR records
-          "1.31.16.172.in-addr.arpa. IN PTR static.1.31.16.172.internal.unsigned-int64.com. 3600"
-        ];
-        remote-control = {
-          control-enable = true;
-          control-interface = ["127.0.0.1"];
-          control-port = 8953;
-        };
-      };
-    };
+    #       # PTR records
+    #       "1.31.16.172.in-addr.arpa. IN PTR static.1.31.16.172.internal.unsigned-int64.com. 3600"
+    #     ];
+    #     remote-control = {
+    #       control-enable = true;
+    #       control-interface = ["127.0.0.1"];
+    #       control-port = 8953;
+    #     };
+    #   };
+    # };
   };
 }
