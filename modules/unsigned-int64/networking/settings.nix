5c04bf893a44967d74fd4be37b2976d8e4658b10
Mariia Holovata <ashuramaru@riseup.net>
2023-11-08 19:16:03 +0200

    partially add dns configuration

--- modules/unsigned-int64/networking/settings.nix
+++ modules/unsigned-int64/networking/settings.nix
@@ -189,17 +189,14 @@
             "\"172.in-addr.arpa.\" static"
           ];
           local-data = [
-            "internal.com. 10800 IN NS internal.com."
-            "internal.com. 10800 IN SOA internal.com. nobody.invalid. 1 3600 1200 604800 10800"
-            "internal.com. 10800 IN A 172.16.31.1"
-            "internal.com. 10800 IN AAAA fd17:216b:31bc:1::1"
+            "\"internal.com. 10800 IN NS internal.com.\""
+            "\"internal.com. 10800 IN SOA internal.com. nobody.invalid. 1 3600 1200 604800 10800\""
+            "\"internal.com. 10800 IN A 172.16.31.1\""
+            "\"internal.com. 10800 IN AAAA fd17:216b:31bc:1::1\""
 
-            "172.in-addr.arpa. 10800 IN NS internal.com."
-            "172.in-addr.arpa. 10800 IN SOA internal.com. nobody.invalid. 2 3600 1200 604800 10800"
-            "1.31.16.172.in-addr.arpa. 10800 IN PTR internal.com."
-          ];
-          local-data-ptr = [
-            "1.31.16.172.in-addr.arpa unsigned-int64.remote"
+            "\"172.in-addr.arpa. 10800 IN NS internal.com.\""
+            "\"172.in-addr.arpa. 10800 IN SOA internal.com. nobody.invalid. 2 3600 1200 604800 10800\""
+            "\"1.31.16.172.in-addr.arpa. 10800 IN PTR internal.com.\""
           ];
         };
         forward-zone = [
