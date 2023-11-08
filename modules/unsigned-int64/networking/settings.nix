8572acfcb374c9d9cfd24268bf2405b9fd48712c
Mariia Holovata <ashuramaru@riseup.net>
2023-11-08 16:33:31 +0200

    add dns zones

--- modules/unsigned-int64/networking/settings.nix
+++ modules/unsigned-int64/networking/settings.nix
@@ -16,12 +16,12 @@
             prefixLength = 27;
           }
         ];
-        # ipv6.addresses = [
-        #   {
-        #     address = "2a01:4f8:141:5330::1";
-        #     prefixLength = 64;
-        #   }
-        # ];
+        ipv6.addresses = [
+          {
+            address = "2a01:4f8:141:5330::";
+            prefixLength = 64;
+          }
+        ];
       };
     };
     defaultGateway = {
