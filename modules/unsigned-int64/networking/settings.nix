9f19ad3195c942b02181d5081e82942cdcc53b88
Mariia Holovata <ashuramaru@riseup.net>
2023-11-08 16:03:03 +0200

    add dns zones

--- modules/unsigned-int64/networking/settings.nix
+++ modules/unsigned-int64/networking/settings.nix
@@ -16,12 +16,12 @@
             prefixLength = 27;
           }
         ];
-        ipv6.addresses = [
-          {
-            address = "2a01:4f8:141:5330::1";
-            prefixLength = 64;
-          }
-        ];
+        # ipv6.addresses = [
+        #   {
+        #     address = "2a01:4f8:141:5330::1";
+        #     prefixLength = 64;
+        #   }
+        # ];
       };
     };
     defaultGateway = {
@@ -152,6 +152,10 @@
           ];
           access-control = [
             "127.0.0.0/8 allow"
+            "172.16.0.0/12 allow"
+            "fd00::/8 allow"
+            "fd17::/16 allow"
+            "fe80::/10 allow"
           ];
           # private-domain = [
           #   "remote.tenjin-dk.com."
