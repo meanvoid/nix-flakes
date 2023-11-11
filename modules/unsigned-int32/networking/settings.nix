b7a1bfa721e17013cde082d7021a1bd6139b7b7b
Mariia Holovata <ashuramaru@riseup.net>
2023-11-11 22:43:41 +0200

    update wireguard key

--- modules/unsigned-int32/networking/settings.nix
+++ modules/unsigned-int32/networking/settings.nix
@@ -74,25 +74,6 @@ in {
   services.wg-netmanager.enable = true;
   networking.wireguard.enable = true;
   networking.wg-quick.interfaces = {
-    wireguard0 = {
-      autostart = false;
-      address = [
-        "192.168.10.100/24"
-        "192.168.254.100/24"
-        "dced:2718:5f06:718a::100/64"
-        "dced:2718:5f06:718a::10/64"
-      ];
-      privateKeyFile = private;
-      peers = [
-        {
-          publicKey = "YnPzt3UDhMI2D6+af+JKhN+5rF1v/imfcDBcZ3k6r0A=";
-          presharedKeyFile = shared;
-          allowedIPs = ["0.0.0.0/0" "::/0"];
-          endpoint = "tenjin-dk.com:51820";
-          persistentKeepalive = 25;
-        }
-      ];
-    };
     wg-ui64 = {
       address = ["172.16.31.2/32" "fd17:216b:31bc:1::2/128"];
       privateKeyFile = private;
@@ -102,15 +83,13 @@ in {
       '';
       peers = [
         {
-          publicKey = "UJNTai8BfRY0w0lYtxyiM+Azcv8rGdWPrPw7Afj1oHk=";
+          publicKey = "";
           presharedKeyFile = shared;
           allowedIPs = [
-            "172.16.31.0/24"
-            "fd17:216b:31bc:1::/64"
             "172.16.31.1/32"
             "fd17:216b:31bc:1::1/128"
           ];
-          endpoint = "tenjin-dk.com:51280";
+          endpoint = "www.tenjin-dk.com:51280";
           persistentKeepalive = 25;
         }
       ];
