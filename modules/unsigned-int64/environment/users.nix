fd5432bb9bda25f8bf84c682b1bf956407b7717d
Mariia Holovata <ashuramaru@riseup.net>
2024-01-11 15:51:24 +0200

    add android user

--- modules/unsigned-int64/environment/users.nix
+++ modules/unsigned-int64/environment/users.nix
@@ -12,6 +12,10 @@
     matthew.gid = config.users.users.matthew.uid;
     jalemi.gid = config.users.users.jalemi.uid;
     solonka.gid = config.users.users.solonka.uid;
+    android.gid = {
+      gid = config.users.users.android.uid;
+      members = ["ashuramaru" "meanrin" "fumono"];
+    };
     minecraft = {
       gid = config.users.users.minecraft.uid;
       members = ["ashuramaru" "fumono" "matthew" "jalemi" "solonka" "nginx"];
@@ -129,6 +133,31 @@
       extraGroups = ["solonka" "docker" "minecraft"];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJoMLTIKqlgWkHGaaNVIzySieiixhld1xB/a7VXuwdrK solonkamay@gmail.com"
+      ];
+    };
+    android = {
+      isNormalUser = true;
+      home = "/Users/android";
+      description = "Android";
+      initialHashedPassword = "";
+      extraGroups = ["podman" "docker"];
+      openssh.authorizedKeys.keys = [
+        # ashuramaru
+        "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBNR1p1OviZgAkv5xQ10NTLOusPT8pQUG2qCTpO3AhmxaZM2mWNePVNqPnjxNHjWN+a/FcZ5on74QZQJtwXI5m80AAAAOc3NoOnJlbW90ZS1kc2E= email:ashuramaru@tenjin-dk.com id:ashuramaru@unsigned-int32"
+        ### --- ecdsa-sk --- ###
+        ### --- ecdsa-sk_bio --- ###
+        "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBFdzdMIdu/bKlIkGx1tCf1sL65NwrmpvBZQ+nSbKknbGHdrXv33mMzLVUsCGUaUxmeYcCULNNtSb0kvgDjRlcgIAAAAOc3NoOnJlbW90ZS1kc2E= email:ashuramaru@tenjin-dk.com id:ashuramaru@unsigned-int32"
+        ### --- ecdsa-sk_bio --- ###
+        ### --- ed25519-sk --- ###
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKNF4qCh49NCn6DUnzOCJ3ixzLyhPCCcr6jtRfQdprQLAAAACnNzaDpyZW1vdGU= email:ashuramaru@tenjin-dk.com id:ashuramaru@unsigned-int32"
+        ### --- ed25519-sk --- ###
+        ### --- ed25519-sk_bio --- ###
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEF0v+eyeOEcrLwo3loXYt9JHeAEWt1oC2AHh+bZP9b0AAAACnNzaDpyZW1vdGU= email:ashuramaru@tenjin-dk.com id:ashuramaru@unsigned-int32"
+        ### --- ed25519-sk_bio --- ###
+        # meanrin
+        "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBCzoNOzhhF9uYDu7CbuzVRJ2K6dClXLrEoJrQvIYjnxHTBMqKuByi9M2HEmkpGO+a3H3WjeeXfqjH2CwZJ97jmIAAAAEc3NoOg== meanrin@outlook.com"
+        # Fumono
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHBeBj6thLiVFNGZI1NuTHKIPvh332Szad2zsgjdzhR mc-server"
       ];
     };
   };
