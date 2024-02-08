36a3141e25e2d3c4b0225d97e325a79477f5cf36
Mariia Holovata <ashuramaru@riseup.net>
2024-02-08 12:37:31 +0200

    update libs

--- modules/unsigned-int32/environment/nvidia.nix
+++ modules/unsigned-int32/environment/nvidia.nix
@@ -25,5 +25,23 @@
   environment.systemPackages = with pkgs; [
     nvtop
     zenith-nvidia
+    cudaPackages_12_0.cudatoolkit
+    cudaPackages_12_0.cudnn
+    linuxPackages.nvidia_x11
+    gcc
+    openblas
+    gnumake
+    cmakeWithGui
+    binutils
+    findutils
   ];
+  environment.sessionVariables = rec {
+    CUDA_HOME = "${pkgs.cudaPackages_12_0.cudatoolkit}";
+    CUDA_PATH = "\${CUDA_HOME}";
+    LD_LIBRARY_PATH = [
+      "\${LD_LIBRARY_PATH}:${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.cudaPackages_12_0.cudnn}/lib"
+    ];
+    EXTRA_LDFLAGS = "-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib";
+    EXTRA_CCFLAGS = "-I/usr/include";
+  };
 }
