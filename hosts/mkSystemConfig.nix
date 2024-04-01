c45c9fa36388d7644a604aed5a09bd7eb3f968d7
Tenjin <ashuramaru@tenjin-dk.com>
2024-04-01 11:27:02 +0200

    add temp;fix for xz backdoor(downgrade to 5.2.9)

--- hosts/mkSystemConfig.nix
+++ hosts/mkSystemConfig.nix
@@ -26,6 +26,19 @@
     nixpkgs.config = {
       allowUnfree = lib.mkDefault true;
     };
+    nixpkgs.overlays = [
+      (final: prev: {
+        # Security: xz-5.6.x is trojaned. See https://github.com/NixOS/nixpkgs/issues/300055
+        xz = prev.xz.overrideAttrs (_: rec {
+          version = "5.2.9";
+
+          src = final.fetchurl {
+            url = "https://tukaani.org/xz/xz-${version}.tar.bz2";
+            sha256 = "sZRQf7o6Rip1PFUxSczaoWgze8t97v3dBnuph8g9/OY=";
+          };
+        });
+      })
+    ];
   };
 in {
   mkSystemConfig = {
