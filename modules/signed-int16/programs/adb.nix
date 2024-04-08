05c2e3b040211db81041ee7dd417798d8545082c
2husecondary <2husecondary.stable254@slmails.com>
2024-04-08 22:39:13 +0400

    add android-udev-rules package
    it kept messing up the permissions i figured i'd add the package but it probably does nothing

--- modules/signed-int16/programs/adb.nix
+++ modules/signed-int16/programs/adb.nix
@@ -6,6 +6,7 @@
 }: {
   environment.systemPackages = [
     pkgs.scrcpy
+    pkgs.android-udev-rules # i'm unsure if i have to do this, but permissions were broken last time i tried to use adb
   ];
   programs.adb.enable = true;
   users.users.reisen.extraGroups = ["adbusers"];
