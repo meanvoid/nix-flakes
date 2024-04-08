0498839ef7401bc8d0638f7f3d15afad2c15bb4b
2husecondary <2husecondary.stable254@slmails.com>
2024-04-08 22:32:12 +0400

    steal openrgb config from nixos wiki

--- modules/signed-int16/environment/openrgb.nix
+++ modules/signed-int16/environment/openrgb.nix
@@ -3,9 +3,33 @@
   config,
   pkgs,
   ...
-}: {
-  services.hardware.openrgb = {
-    enable = true;
-    motherboard = "intel";
+}: let
+  yuyuko = pkgs.writeScriptBin "yuyuko" ''
+    #!/bin/sh
+    NUM_DEVICES=$(${pkgs.openrgb}/bin/openrgb --list-devices | grep -E '^[0-9]+: ' | wc -l)
+
+    for i in $(seq 0 $(($NUM_DEVICES - 1))); do
+      ${pkgs.openrgb}/bin/openrgb --device $i --mode direct --color fc3a3a
+    done
+  '';
+in {
+  # I STOLE FROM THE WIKI AGAIN I AM IN SHAMBLES DUE TO MY OWN INCOMPETENCE
+  config = {
+    services.udev.packages = [pkgs.openrgb];
+    boot.kernelModules = ["i2c-dev"];
+    hardware.i2c.enable = true;
+    services.hardware.openrgb = {
+      enable = true;
+      package = pkgs.openrgb-with-all-plugins;
+      motherboard = "intel";
+    };
+    systemd.services.yuyuko = {
+      description = "yuyuko";
+      serviceConfig = {
+        ExecStart = "${yuyuko}/bin/yuyuko";
+        Type = "oneshot";
+      };
+      wantedBy = ["multi-user.target"];
+    };
   };
 }
