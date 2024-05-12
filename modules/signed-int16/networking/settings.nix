386acffe35f0321af008ca37b3aeeda3b3ede5a7
Tenjin <ashuramaru@tenjin-dk.com>
2024-05-12 15:49:17 +0300

    signed-int16: add wireguard configuration
    secrets: add secrets for signed-int16's wireguard configuration

--- modules/signed-int16/networking/settings.nix
+++ modules/signed-int16/networking/settings.nix
@@ -1,4 +1,14 @@
-{ hostname, ... }:
+{
+  pkgs,
+  config,
+  hostname,
+  path,
+  ...
+}:
+let
+  private = config.age.secrets.wireguard-client_fumono.path;
+  shared = config.age.secrets.wireguard-shared_fumono.path;
+in
 {
   networking = {
     hostName = "${hostname}";
@@ -38,5 +48,35 @@
         port = 22;
       }
     ];
+  };
+  age.secrets = {
+    wireguard-client.file = path + /secrets/wireguard-client.age;
+    wireguard-shared.file = path + /secrets/wireguard-shared.age;
+  };
+  services.wg-netmanager.enable = true;
+  networking.wireguard.enable = true;
+  networking.wg-quick.interfaces = {
+    wg-ui64 = {
+      address = [
+        "172.16.31.10/32"
+        "fd17:216b:31bc:1::10/128"
+      ];
+      privateKeyFile = private;
+      postUp = ''
+        ${pkgs.systemd}/bin/resolvectl dns wg-ui64 172.16.31.1
+        ${pkgs.systemd}/bin/resolvectl domain wg-ui64 ~tenjin.com ~internal.com ~\rcon.fumoposting.com
+      '';
+      peers = [
+        {
+          publicKey = "X6OBa2aMpoLGx9lYSa+p1U8OAx0iUxAE6Te9Mucu/HQ=";
+          presharedKeyFile = shared;
+          allowedIPs = [
+            "172.16.31.1/24"
+            "fd17:216b:31bc:1::1/128"
+          ];
+          endpoint = "www.tenjin-dk.com:51280";
+        }
+      ];
+    };
   };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,26 +4,30 @@ let
     meanrin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFS10V6yIxDfxjepo1jmYam1qQ67NMpkPeUFDJokhOJj";
   };
   systems = {
+    signed-int16 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHBeBj6thLiVFNGZI1NuTHKIPvh332Szad2zsgjdzhR";
     unsigned-int32 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJQV9RnBPh3xOoh7fhaxS74YfgTH6BgqV9zsTSVdiWDI root@unsigned-int32";
     unsigned-int64 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdiVn6zgj+VGj3BIGiwMFH3AumoGSCzaVukcbQWVz1K root@unsigned-int64";
   };
-  matrix = [
+  matrixTenjin = [
     systems.unsigned-int32
+    systems.unsigned-int64
+  ];
+  matrixFumono = [
+    systems.signed-int16
     systems.unsigned-int64
   ];
 in
 {
+  # signed-int16
+  "wireguard-client_fumono.age".publicKeys = [ systems.signed-int16 ];
+  "wireguard-shared_fumono.age".publicKeys = matrixFumono;
+
+  # unsigned-int32
   "foldingathome_passkey.age".publicKeys = [ systems.unsigned-int32 ];
   "foldingathome_token.age".publicKeys = [ systems.unsigned-int32 ];
   "wireguard-client.age".publicKeys = [ systems.unsigned-int32 ];
-  "wireguard-server.age".publicKeys = matrix;
-  "wireguard0-server.age".publicKeys = matrix;
-  "wireguard-shared.age".publicKeys = matrix;
-  "wireguard-shared_fumono.age".publicKeys = matrix;
-  "wireguard-shared_jul.age".publicKeys = matrix;
-  "tailscale-auth-key.age".publicKeys = matrix;
-  "cert.age".publicKeys = matrix;
 
+  # unsigned-int64
   "mail.age".publicKeys = [ systems.unsigned-int64 ];
   "proxy.age".publicKeys = [ systems.unsigned-int64 ];
   "admin.age".publicKeys = [ systems.unsigned-int64 ];
@@ -34,4 +38,12 @@ in
   "vault.age".publicKeys = [ systems.unsigned-int64 ];
   "prometheus.age".publicKeys = [ systems.unsigned-int64 ];
   "transmission_openvpn-env.age".publicKeys = [ systems.unsigned-int64 ];
+
+  # matrixTenjin
+  "wireguard-server.age".publicKeys = matrixTenjin;
+  "wireguard0-server.age".publicKeys = matrixTenjin;
+  "wireguard-shared.age".publicKeys = matrixTenjin;
+  "wireguard-shared_jul.age".publicKeys = matrixTenjin;
+  "tailscale-auth-key.age".publicKeys = matrixTenjin;
+  "cert.age".publicKeys = matrixTenjin;
 }
