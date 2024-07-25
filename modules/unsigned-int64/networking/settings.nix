64f194f2a976d62f46833aa3864fdfc18b9c5562
Tenjin <ashuramaru@tenjin-dk.com>
2024-07-25 22:58:39 +0300

    unsigned-int64: PLEASE JUST FUCKING WORK

--- modules/unsigned-int64/networking/settings.nix
+++ modules/unsigned-int64/networking/settings.nix
@@ -3,8 +3,8 @@
   networking = {
     hostName = "unsigned-int64";
     interfaces = {
-      "eth1" = {
-        name = "eth1";
+      "eth0" = {
+        name = "eth0";
         useDHCP = lib.mkDefault true;
         ipv4.addresses = [
           {
@@ -27,18 +27,18 @@
           }
         ];
       };
-      "eth0" = {
-        name = "eth0";
-        useDHCP = lib.mkDefault true;
+      "eth1" = {
+        name = "eth1";
+        useDHCP = lib.mkDefault false;
       };
     };
     defaultGateway = {
       address = "116.202.39.1";
-      interface = "eth1";
+      interface = "eth0";
     };
     defaultGateway6 = {
       address = "fe80::1";
-      interface = "eth1";
+      interface = "eth0";
     };
     nameservers = [
       "127.0.0.1"
@@ -47,7 +47,7 @@
     nat = {
       enable = true;
       enableIPv6 = true;
-      externalInterface = "eth1";
+      externalInterface = "eth0";
       internalInterfaces = [
         "ve-+"
         "virbr0"
