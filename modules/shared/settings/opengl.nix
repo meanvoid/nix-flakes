f90217b4047d017ec3b23239c1942d965eac3411
Tenjin <ashuramaru@tenjin-dk.com>
2024-07-31 10:54:53 +0300

    fixup

--- modules/shared/settings/opengl.nix
+++ modules/shared/settings/opengl.nix
@@ -4,22 +4,15 @@
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
-    extraPackages = builtins.attrValues {
-      inherit (pkgs)
-        # VAAPI
-        libva
-        vaapiVdpau
-        libvdpau-va-gl
-        ;
-      nvidia-vaapi = if config.hardware.nvidia.modesetting.enable then pkgs.nvidia-vaapi-driver else null;
-      egl-wayland = if config.hardware.nvidia.modesetting.enable then pkgs.egl-wayland else null;
-    };
-    extraPackages32 = builtins.attrValues {
-      inherit (pkgs.driversi686Linux)
-        # VAAPI
-        vaapiVdpau
-        libvdpau-va-gl
-        ;
-    };
+    extraPackages = builtins.filter (pkg: pkg != null) (
+      builtins.attrValues {
+        inherit (pkgs) libva vaapiVdpau libvdpau-va-gl;
+        nvidia-vaapi = if config.hardware.nvidia.modesetting.enable then pkgs.nvidia-vaapi-driver else null;
+        egl-wayland = if config.hardware.nvidia.modesetting.enable then pkgs.egl-wayland else null;
+      }
+    );
+    extraPackages32 = builtins.filter (pkg: pkg != null) (
+      builtins.attrValues { inherit (pkgs.driversi686Linux) vaapiVdpau libvdpau-va-gl; }
+    );
   };
 }
