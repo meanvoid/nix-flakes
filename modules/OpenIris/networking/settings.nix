6e8c54979e040c96c38ab112f2cf0713a6210733
jalemi <snikersdaun@gmail.com>
2024-11-22 02:15:17 +1000

    trying to add a custom option

--- modules/OpenIris/networking/settings.nix
+++ modules/OpenIris/networking/settings.nix
@@ -1,5 +1,36 @@
-{ hostname, ... }:
+{ hostname, lib, ... }:
 {
-  networking.networkmanager.enable = true;
-  networking.hostName = hostname;
+  networking = {
+    networkmanager.enable = true;
+    networkmanager.dns = "none";
+    dhcpcd.extraConfig = "nohook resolv.conf";
+    hostName = hostname;
+    nameservers = [
+      "127.0.0.1"
+      "::1"
+    ];
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        #xdebug
+        9003
+        # HTTP
+        80
+        # HTTPS
+        443
+      ];
+    };
+  };
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = true;
+      doh_servers = false;
+      odoh_servers = true;
+      require_dnssec = true;
+    };
+    upstreamDefaults = true;
+  };
+  systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+  systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
 }
--- modules/OpenIris/networking/zapret.nix
+++ modules/OpenIris/networking/zapret.nix
@@ -1,41 +1,24 @@
 { inputs, ... }:
 {
-  imports = [ inputs.zapret.nixosModules.zapret ];
-  services.zapret.enable = true;
-  services.zapret.config = ''
-    FWTYPE=nftables
-    SET_MAXELEM=522288
-    IPSET_OPT="hashsize 262144 maxelem $SET_MAXELEM"
-    IP2NET_OPT4="--prefix-length=22-30 --v4-threshold=3/4"
-    IP2NET_OPT6="--prefix-length=56-64 --v6-threshold=5"
-    AUTOHOSTLIST_RETRANS_THRESHOLD=3
-    AUTOHOSTLIST_FAIL_THRESHOLD=3
-    AUTOHOSTLIST_FAIL_TIME=60
-    AUTOHOSTLIST_DEBUGLOG=0
-    MDIG_THREADS=30
+  disabledModules = [ "services/networking/zapret.nix" ];
+  imports = [ "${inputs.nixpkgs-zapret-upstream}/nixos/modules/services/networking/zapret.nix" ];
+  services.zapret = {
+    enable = true;
+    udpSupport = true;
+    udpPorts = [
+      "50000-65535"
+    ];
+    params = [
+      "--filter-udp=443,9993,50000-65535"
+      "--dpi-desync=fake"
+      "--dpi-desync-any-protocol"
+      "--dpi-desync-autottl=2"
+      "--new"
+      "--filter-tcp=443"
+      "--dpi-desync=fake,disorder"
+      "--dpi-desync-ttl=4"
+      "--dpi-desync-any-protocol"
+    ];
+  };
 
-    GZIP_LISTS=1
-
-    MODE=nfqws
-    MODE_HTTP=1
-    MODE_HTTP_KEEPALIVE=0
-    MODE_HTTPS=1
-    MODE_QUIC=1
-    MODE_FILTER=none
-
-    QUIC_PORTS=9993,50000-65535
-
-
-    DESYNC_MARK=0x40000000
-    DESYNC_MARK_POSTNAT=0x20000000
-    NFQWS_OPT_DESYNC="--dpi-desync=disorder2"
-    NFQWS_OPT_DESYNC_QUIC="--dpi-desync=fake --dpi-desync-repeats=8 --dpi-desync-any-protocol --dpi-desync-autottl=1:1-8"
-
-    TPWS_OPT="--hostspell=HOST --split-http-req=method --split-pos=3 --oob"
-
-    FLOWOFFLOAD=donttouch
-
-    INIT_APPLY_FW=1
-    DISABLE_IPV6=0
-  '';
 }
