d5974f4912253684a725f311fa1a686025331ef0
jalemi <snikersdaun@gmail.com>
2024-11-22 10:20:36 +1000

    changed some confings in zapret

--- modules/OpenIris/networking/zapret.nix
+++ modules/OpenIris/networking/zapret.nix
@@ -6,15 +6,9 @@
     enable = true;
     udpSupport = true;
     udpPorts = [
-      "50000-65535"
+      "50000:65535"
     ];
     params = [
-      "--filter-udp=443,9993,50000-65535"
-      "--dpi-desync=fake"
-      "--dpi-desync-any-protocol"
-      "--dpi-desync-autottl=2"
-      "--new"
-      "--filter-tcp=443"
       "--dpi-desync=fake,disorder"
       "--dpi-desync-ttl=4"
       "--dpi-desync-any-protocol"
